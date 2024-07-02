2f185d90a29b5b42a7af9126f933eb347c6c3bb3
Danis Music <danis200004@icloud.com>
2024-07-02 22:18:34 +0200

    Add methods to find and compare JSON chapters and scraped chapters

--- scraper/src/main/java/com/example/FicScraper.java
+++ scraper/src/main/java/com/example/FicScraper.java
@@ -1,21 +1,26 @@
 package com.example;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class FicScraper {
     private String urlOfFic;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
 
     public FicScraper(String urlOfFic) {
         this.urlOfFic = urlOfFic;
     }
-
-  
 
     public String getUrlOfFic() {
         return this.urlOfFic;
@@ -38,7 +43,17 @@ public class FicScraper {
 
     public Fiction ficInformation() {
         Fiction fic = null;
+        String filePath = "scraper/src/main/java/com/example/fics.json";  
+
+        
         try {
+            FileInputStream fileInputStream = new FileInputStream(new File(filePath));
+            
+            JsonNode rootNode = objectMapper.readTree(fileInputStream);
+               
+            JsonNode fictionsArray = rootNode.path("fictions");
+            int ficID = fictionsArray.size() + 1;
+
             Document document = Jsoup.connect(this.urlOfFic).get();
             Elements titleAndAuthorContainer = document.select(".col");
             
@@ -63,13 +78,27 @@ public class FicScraper {
             System.out.println("Description: " + description);
             System.out.println("===============================================");
 
-            fic = new Fiction(title, author, chapterAmount, description);
+            fic = new Fiction(getUrlOfFic(),ficID, title, author, chapterAmount, description);
         } catch (Exception e) {
             // TODO: handle exception
         }
         return fic;
     }
 
+
+    public String searchForChap(){
+        String output = "";
+        try {
+            Document document = Jsoup.connect(this.urlOfFic).get();
+            output = document.select("div.portlet.light > div.portlet-title > div.actions > span").text();
+            String parsedOutput = output.split(" ")[0];
+            return parsedOutput;
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+        return output;
+    }
 
     public void searchForTitle(Elements wrapper) {
         for (Element potato : wrapper) {
@@ -79,6 +108,17 @@ public class FicScraper {
             System.out.println("=========================================");
         }
     }
+
+    public String checkUpdatedChap(int ficID){
+        JsonDeserializer jsonDeserializer = new JsonDeserializer();
+        if (Integer.parseInt(jsonDeserializer.getChapAmountInJSON(ficID)) < Integer.parseInt(searchForChap())) {
+            return String.format("New chapter found!");
+        } else {
+            return String.format("No new chapter for this fic.");
+        }
+    }
+
+
 }
 
 
