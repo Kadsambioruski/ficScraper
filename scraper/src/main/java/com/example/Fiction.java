35f41de341d37973a5f58b32308d81a39db9b5fa
Danis Music <danis200004@icloud.com>
2024-07-02 22:16:52 +0200

    Add new metadata for easier manipulation of JSON file

--- scraper/src/main/java/com/example/Fiction.java
+++ scraper/src/main/java/com/example/Fiction.java
@@ -1,18 +1,31 @@
 package com.example;
 
 public class Fiction {
+    String ficLink;
+    int ficID;
     String title;
     String author;
     String chapAmount;
     String description;
 
+
     public Fiction(){}
 
-    public Fiction(String title, String author, String chapAmount, String description) {
+    public Fiction(String ficLink, int ficID, String title, String author, String chapAmount, String description) {
+        this.ficLink = ficLink;
+        this.ficID = ficID;
         this.title = title;
         this.author = author;
         this.chapAmount = chapAmount;
         this.description = description;
+    }
+
+    public String getFicLink() {
+        return this.ficLink;
+    }
+
+    public int getFicID() {
+        return this.ficID;
     }
 
     public String getTitle() {
