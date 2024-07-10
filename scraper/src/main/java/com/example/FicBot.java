486eb9fe1e2915755ec6aee5ec65d1d1a9602d02
Danis Music <danis200004@icloud.com>
2024-07-10 12:50:32 +0200

    Add methods to handle slash commands from user in discord

--- scraper/src/main/java/com/example/FicBot.java
+++ scraper/src/main/java/com/example/FicBot.java
@@ -2,17 +2,25 @@ package com.example;
 import discord4j.common.util.Snowflake;
 import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
 import discord4j.core.event.domain.lifecycle.ReadyEvent;
+import discord4j.core.object.command.ApplicationCommandInteractionOption;
+import discord4j.core.object.command.ApplicationCommandInteractionOptionValue;
+import discord4j.core.object.command.ApplicationCommandOption;
 import discord4j.core.object.entity.User;
 import discord4j.core.object.entity.channel.MessageChannel;
+import discord4j.discordjson.json.ApplicationCommandData;
+import discord4j.discordjson.json.ApplicationCommandOptionData;
+import discord4j.discordjson.json.ApplicationCommandRequest;
 import discord4j.rest.entity.RestChannel;
 import reactor.core.publisher.Mono;
 
 public class FicBot {
-    
-    public static GatewayDiscordClient login(String token) {
-        DiscordClient client = DiscordClient.create(token); 
-        return client.login().block();
+    //private final static long serverId = 1259531516854276156L;
+    private static String token = "MTI1OTUzMDI4MDMzNTU3NzE3MA.G8JiDR.D2Ed5Kqy3WhXSSBGQRiLI2B9Ku3EUF6ybjIuoU";
+    private static long serverId = 1259531516854276156L;
+    public static GatewayDiscordClient  login(String token) {
+        return DiscordClient.create(token).login().block();
     }
 
 
@@ -23,5 +31,54 @@ public class FicBot {
             .ofType(MessageChannel.class)
             .flatMap(channel -> channel.createMessage(message))
             .then().block();
+    }
+
+    public static Mono<ApplicationCommandData> registerSlashCommand(GatewayDiscordClient gateway) {
+
+        long applicationId = FicBot.login(token).getRestClient().getApplicationId().block();
+
+        ApplicationCommandOptionData optionData = ApplicationCommandOptionData.builder()
+            .name("name")
+            .description("Name of the fic you want to have latest chap updated")
+            .type(ApplicationCommandOption.Type.STRING.getValue())
+            .required(true)
+            .build();
+            
+        ApplicationCommandRequest commandRequest = ApplicationCommandRequest.builder()
+            .name("read")
+            .description("Tells the bot that the chapter for the specific fic has been read")
+            .addOption(optionData)
+            .build();
+
+            return gateway.getRestClient().getApplicationService()
+                .createGlobalApplicationCommand(applicationId, commandRequest)
+                .onErrorResume(e -> {
+                    e.printStackTrace();
+                    return Mono.empty();
+                });
+    }
+
+    public static Mono<Void> handleReadCommand(ChatInputInteractionEvent event) {
+        String reply;
+        JsonDeserializer jsonDeserializer = new JsonDeserializer();
+
+        System.out.println("Interaction Event Details:");
+        System.out.println("Command Name: " + event.getCommandName());
+        System.out.println("Options: " + event.getOptions().toString());
+
+        String nameOfFic = event.getOption("name")
+            .flatMap(ApplicationCommandInteractionOption::getValue)
+            .map(ApplicationCommandInteractionOptionValue::asString)
+            .orElse("Unknown Fic");
+
+        System.out.println("Recieved name of fic: " + nameOfFic);
+        if (jsonDeserializer.matchFicTitle(nameOfFic)) {
+            jsonDeserializer.setFicChapter(nameOfFic);
+            reply = "Latest chapter for " + nameOfFic + "has been updated!";
+        } else {
+            reply = "Could not find a fic in file that matches the name provided.";
+        }
+
+        return event.reply(reply).then();
     }
 }
--- scraper/src/main/java/com/example/Main.java
+++ scraper/src/main/java/com/example/Main.java
@@ -5,104 +5,103 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.sql.Time;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
-
-import javax.sound.midi.Soundbank;
+import java.util.concurrent.TimeUnit;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
+import discord4j.discordjson.json.ApplicationCommandData;
+import reactor.core.publisher.Mono;
 
 public class Main {
     private static Scanner userInputScanner = new Scanner(System.in);
     private final static String MENU_ITEM_Q = "-1";
     private static String token = "";
     private static String generalChat = "";
+    private static JsonDeserializer jsonDeserializer = new JsonDeserializer();
+    private static boolean runLoop = true;
 
     public static void main(String[] args) {
         boolean notQ = true;
-        JsonDeserializer jsonDeserializer = new JsonDeserializer();
+        GatewayDiscordClient gateWay1 = FicBot.login(token);
 
-        //TODO use cookie saving for cloudflare
+        long applicationId = gateWay1.getRestClient().getApplicationId().block();
+
+        if (gateWay1 != null) {
+            Mono<ApplicationCommandData> brotato = FicBot.registerReadCommand(gateWay1, applicationId);
+            Mono<ApplicationCommandData> smotato = FicBot.registerEndLoopCommand(gateWay1, applicationId);
+            
+            brotato.doOnSuccess(commandData -> {
+                if (commandData != null) {
+                    System.out.println("Read command registered successfully: " + commandData.name());
+                } else {
+                    System.err.println("Failed to register read command.");
+                }
+
+            }).subscribe();
+
+            smotato.doOnSuccess(commandData -> {
+                if (commandData != null) {
+                    System.out.println("endloop command registered successfully: " + commandData.name());
+                } else {
+                    System.err.println("Failed to register endloop command.");
+                }
+
+            }).subscribe();
+
+        }
+
+
+        
+        Runnable potato = () -> {
+            FicBot.sendMessage(gateWay1, generalChat, String.format("Listening for commands!"));
+            recieveDiscCommand(gateWay1);
+        };
+        Thread discordListenThread = new Thread(potato);
+
+        discordListenThread.start();
+
+                
+        
+        // Handle command interactions
+        // TODO use cookie saving for cloudflare
         // Currently only works with Royal Road
         while (notQ) {
             try {
                 int menuNav = Integer.parseInt(menu());
                 switch (menuNav) {
                     case 1:
-                        System.out.println("Put in link to new fic: ");
-                        String newLinkToFic = input();
-                        //TODO parse through input to check link format
-                        FicScraper ficScraper = new FicScraper(newLinkToFic);
-                        Fiction newFic = ficScraper.ficInformation();
-                        
-                        JsonSerializer serializer = new JsonSerializer();
-                        serializer.saveFicToJson(newFic);
-                        
-                        System.out.println("Fic added to JSON file: " + newFic);
-                        break;
+                        putFicInJson();
+                    break;
                     case 2:
                         jsonDeserializer.readJsonFile();
-                        break;
+                    break;
                     case 3:
-                        System.out.println("Choose fic from list through fic Number");
-                        String ficNumberString = input();
-                        int parsedFicNumber = Integer.parseInt(ficNumberString);
-
-                        FicScraper ficScraper2 = new FicScraper(jsonDeserializer.getFicLink(parsedFicNumber));
-
-                        System.out.println(jsonDeserializer.getChapAmountInJSON(parsedFicNumber));
-                        
-                        if (ficScraper2.checkUpdatedChap(parsedFicNumber) != null) {
-                            GatewayDiscordClient gateWay = FicBot.login(token);
-                            FicBot.sendMessage(gateWay, generalChat, ficScraper2.checkUpdatedChap(parsedFicNumber));
-                        }
-                        break;
-                    case 4:
-                        System.out.println("Put in the amount of hours the loop should run: ");
-                        int workHours = Integer.parseInt(input());
-                        String filePath = "scraper/src/main/java/com/example/fics.json";
-
-                        LocalDateTime startTime = LocalDateTime.now();
-                        LocalDateTime endTime = startTime.plusHours(workHours);
-                        while (LocalDateTime.now().isBefore(endTime)) {
-                            try (FileInputStream fileInputStream = new FileInputStream(new File(filePath));
-                            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, StandardCharsets.UTF_8)){ // Translates the text to UTF_8
-                            
-                                ObjectMapper objectMapper = new ObjectMapper();
-                            
-                                JsonNode rootNode = objectMapper.readTree(inputStreamReader);
-                    
-                                JsonNode fictionsArray = rootNode.path("fictions");
-                                
-                                if (fictionsArray.isArray()) {
-                                    for (JsonNode fictionNode : fictionsArray) {
-                                        int ficId = Integer.parseInt(fictionNode.path("ficID").asText());
-                                        FicScraper ficScraper3 = new FicScraper(jsonDeserializer.getFicLink(ficId));
-                                       System.out.println(ficScraper3.checkUpdatedChap(ficId));
-                                        
-                                    }
-                                }
-                                
-                            } catch (IOException e) {
-                                e.printStackTrace();        
-                            }
-                        }
+                        checkFicUpdates();
+                    break;
+                    case 4: 
+                        scrapeFicLoop(gateWay1);
                         break;
                     case -1:
                         notQ = false;
                         break;
                 }
-                
+
             } catch (NumberFormatException e) {
                 System.out.println("Input was not an integer or q, try again.");
                 e.printStackTrace();
             }
 
         }
-        
+
     }
 
     public static String input() {
@@ -113,13 +112,12 @@ public class Main {
             if (input.equals("q")) {
                 inputValue = MENU_ITEM_Q;
                 break;
-            } 
+            }
             inputValue = input;
             break;
-        }     
+        }
         return inputValue;
     }
-
 
     public static String menu() {
         System.out.println("""
@@ -127,10 +125,125 @@ public class Main {
                 2. See all fictions
                 3. New updates from fics
                 4. Turn on loop
-                q. Quit 
+                q. Quit
                 """);
-        
+
         return input();
+    }
+
+
+    public static void recieveDiscCommand(GatewayDiscordClient gateway){
+        gateway.on(ChatInputInteractionEvent.class)
+            .flatMap(event -> {
+                switch (event.getCommandName()) {
+                    case "read":
+                        return FicBot.handleReadCommand(event);
+                    case "endloop":
+                        runLoop = false;
+                        return FicBot.handleEndLoopCommand(event);
+                    default:
+                    return Mono.empty();
+                }
+            })
+            .onErrorResume(err -> {
+                err.printStackTrace();
+                return Mono.empty();
+            })
+            .then(gateway.onDisconnect()) // Chaining with the disconnect operation
+            .block(); // Block until completion
+    }
+
+    public static void putFicInJson(){
+        System.out.println("Put in link to new fic: ");
+        String newLinkToFic = input();
+        // TODO parse through input to check link format
+        FicScraper ficScraper = new FicScraper(newLinkToFic);
+        Fiction newFic = ficScraper.ficInformation();
+        
+        JsonSerializer serializer = new JsonSerializer();
+        serializer.saveFicToJson(newFic);
+        
+        System.out.println("Fic added to JSON file: " + newFic);
+    }
+
+    public static void checkFicUpdates(){
+        System.out.println("Choose fic from list through fic Number");
+        String ficNumberString = input();
+        int parsedFicNumber = Integer.parseInt(ficNumberString);
+        
+        FicScraper ficScraper2 = new FicScraper(jsonDeserializer.getFicLink(parsedFicNumber));
+        
+        System.out.println(jsonDeserializer.getChapAmountInJSON(parsedFicNumber));
+        
+        if (ficScraper2.checkUpdatedChap(parsedFicNumber)) {
+            GatewayDiscordClient gateWay = FicBot.login(token);
+            FicBot.sendMessage(gateWay, generalChat,
+            String.format("New chapter found! Latest chapter in "
+            + jsonDeserializer.getFicTitle(parsedFicNumber) + " is: "
+            + Integer.parseInt(ficScraper2.searchForChap())));
+        }
+    }
+
+    //TODO could use a discord command to end the webscrape loop instead of deactivating it through the terminal (could also try to start it with a command although i have no clue how rn)
+    public static void scrapeFicLoop(GatewayDiscordClient gateWay){
+        LocalDateTime startTime = LocalDateTime.now();
+        System.out.println("Loop has started!");
+        String filePath = "scraper/src/main/java/com/example/fics.json";
+        Map<Integer, Integer> lastSeenChapters = new HashMap<>();
+        
+        LocalDateTime nextTime = LocalDateTime.now().withMinute(0).withSecond(0);
+        
+        while (runLoop) {
+            if(LocalDateTime.now().withMinute(0).withSecond(0).isBefore(nextTime)) {
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    // TODO: handle exception
+                }
+                continue;
+            } else {
+                nextTime = LocalDateTime.now().plusHours(3);
+            }
+            try (FileInputStream fileInputStream = new FileInputStream(new File(filePath));
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream,
+            StandardCharsets.UTF_8)) { // Translates the text to UTF_8
+                
+                
+                ObjectMapper objectMapper = new ObjectMapper();
+                
+                JsonNode rootNode = objectMapper.readTree(inputStreamReader);
+                
+                JsonNode fictionsArray = rootNode.path("fictions");
+
+                if (fictionsArray.isArray()) {
+                    for (JsonNode fictionNode : fictionsArray) {
+                        int ficId = Integer.parseInt(fictionNode.path("ficID").asText());
+                        FicScraper ficScraper3 = new FicScraper(jsonDeserializer.getFicLink(ficId));
+                        int latestChapter = Integer.parseInt(ficScraper3.searchForChap());
+                        
+                        if (ficScraper3.checkUpdatedChap(ficId)) {
+                            if (!lastSeenChapters.containsKey(ficId)|| lastSeenChapters.get(ficId) < latestChapter) {
+                                
+                                lastSeenChapters.put(ficId, latestChapter);
+                                
+                                
+                                FicBot.sendMessage(gateWay, generalChat,
+                                String.format("New chapter found! Latest chapter in "
+                                + jsonDeserializer.getFicTitle(ficId) + " is: "
+                                + Integer.parseInt(ficScraper3.searchForChap())));
+                            }
+                        }
+                    }
+                }
+                
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("nfgegeognegoeg");
+        LocalDateTime endTime = LocalDateTime.now();
+        long totalTime = startTime.until(endTime, ChronoUnit.HOURS);
+        System.out.println("Uptime of loop: " + totalTime + "hours.");
     }
     
 }
